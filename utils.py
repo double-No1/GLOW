b17fb1636f5f573fc30953f2780f56e039280dc1
double12345 <88550249+double-No1@users.noreply.github.com>
2025-03-15 11:51:28 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,9 +7,5 @@ def compute_pre_recall_f1(target, score):
     pred[score > threshold] = 1
     precision, recall, f1, _ = precision_recall_fscore_support(target, pred, average='binary')
 
-    # precision, recall, thresholds = precision_recall_curve(target, score)
-    # numerator = 2 * recall * precision
-    # denom = recall + precision
-    # f1_scores = np.divide(numerator, denom, out=np.zeros_like(denom), where=(denom != 0))
-    # f1 = np.max(f1_scores)
+
     return f1
