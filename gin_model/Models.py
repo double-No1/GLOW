02a74b71a7cdbd4ad3b87c0ce1556396d6bb8936
double12345 <88550249+double-No1@users.noreply.github.com>
2025-03-15 11:41:35 +0800

    Update Models.py

--- gin_model/Models.py
+++ gin_model/Models.py
@@ -1,19 +1,3 @@
-# Raising the Bar in Graph-level Anomaly Detection (GLAD)
-# Copyright (c) 2022 Robert Bosch GmbH
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published
-# by the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 import torch.nn as nn
 import torch
 from Code.gin_model.GraphNets import GIN,GraphNorm,GIN_classifier,GINConv
@@ -27,7 +11,7 @@ from torch_geometric.nn import global_add_pool, global_mean_pool,global_max_pool
 class OCGIN(nn.Module):
     def __init__(self, dim_features, config):
         super(OCGIN, self).__init__()
-        self.dropout = nn.Dropout(p=0.5)  # 添加dropout层
+        self.dropout = nn.Dropout(p=0.5) 
         self.dim_targets = config['hidden_dim']
         self.num_layers = config['num_layers']
         self.device = config['device']
